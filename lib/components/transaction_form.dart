476322d7c0107ad82e1d5a54aa3b2555a1d86d57
João Pedro Ternes Conte <joaopedrotc04@gmail.com>
2023-07-10 15:12:43 -0300

    refactor: refactored the form component

--- lib/components/transaction_form.dart
+++ lib/components/transaction_form.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 
 class TransactionForm extends StatelessWidget {
-  TransactionForm({super.key});
 
   final titleController = TextEditingController();
   final valueController = TextEditingController();
+
+  final void Function(String, double) onSubmit;
+
+  TransactionForm(this.onSubmit, {super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -36,8 +39,9 @@ class TransactionForm extends StatelessWidget {
                   ),
                   child: const Text("New Transaction"),
                   onPressed: () {
-                    print(titleController.text);
-                    print(valueController.text);
+                    final title = titleController.text;
+                    final value = double.tryParse(valueController.text) ?? 0.0;
+                    onSubmit(title, value);
                   },
                 ),
               ],
