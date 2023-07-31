509cc8b3f7cc7476f7e7af79b9c6b71e044266ad
João Pedro Ternes Conte <joaopedrotc04@gmail.com>
2023-07-31 20:24:08 -0300

    style: style changes

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -4,73 +4,70 @@ import 'package:intl/intl.dart';
 
 class TransactionList extends StatelessWidget {
   final List<Transaction> transactions;
+  final void Function(String) onRemove;
 
-  const TransactionList(this.transactions, {Key? key}) : super(key: key);
+  const TransactionList(this.transactions, this.onRemove, {Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: 300,
-      child: transactions.isEmpty ? Column(
+      height: 430,
+      child: transactions.isEmpty
+        ? Column(
         children: [
-          const SizedBox(height: 20,),
-          Text(
-            "No Transaction Registered",
-            style: Theme.of(context).textTheme.titleLarge,
-          ),
-          const SizedBox(height: 20,),
-          SizedBox(
-            height: 200,
-            child: Image.asset(
-              "assets/images/waiting.png",
-              fit: BoxFit.cover,
+            const SizedBox(height: 20),
+            Text(
+              "No Transaction Registered",
+              style: Theme.of(context).textTheme.titleLarge,
             ),
-          ),
-        ],
-      ) : ListView.builder(
+            const SizedBox(height: 20),
+            SizedBox(
+              height: 200,
+              child: Image.asset(
+                "assets/images/waiting.png",
+                fit: BoxFit.cover,
+              ),
+            ),
+          ],
+        )
+      : ListView.builder(
         itemCount: transactions.length,
         itemBuilder: (ctx, index) {
           final tr = transactions[index];
           return Card(
-            child: Row(
-              children: [
-                Container(
-                  margin: const EdgeInsets.symmetric(
-                    horizontal: 15,
-                    vertical: 10,
-                  ),
-                  decoration: BoxDecoration(
-                    border: Border.all(
-                      color: Theme.of(context).colorScheme.primary,
-                      width: 2,
-                    ),
-                  ),
-                  padding: const EdgeInsets.all(10),
-                  child: Text(
-                    "R\$ ${tr.value.toStringAsFixed(2)}",
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20,
-                      color: Theme.of(context).colorScheme.primary,
-                    ),
-                  ),
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      tr.title,
-                      style: Theme.of(context).textTheme.titleLarge
-                    ),
-                    Text(
-                      DateFormat("d MMM y").format(tr.date),
+            elevation: 5,
+            margin: const EdgeInsets.symmetric(
+              vertical: 8,
+              horizontal: 5,
+            ),
+            child: ListTile(
+              leading: CircleAvatar(
+                backgroundColor: Colors.green,
+                radius: 30,
+                child: Padding(
+                  padding: const EdgeInsets.all(6),
+                  child: FittedBox(
+                    child: Text(
+                      "R\$${tr.value}",
                       style: const TextStyle(
-                        color: Colors.grey,
+                        color: Colors.white,
                       ),
                     ),
-                  ],
+                  ),
                 ),
-              ],
+              ),
+              title: Text(
+                tr.title,
+                style: Theme.of(context).textTheme.titleLarge,
+              ),
+              subtitle: Text(
+                DateFormat("d MMM y").format(tr.date)
+              ),
+              trailing: IconButton(
+                icon: const Icon(Icons.delete),
+                color: Theme.of(context).colorScheme.error,
+                onPressed: () => onRemove(tr.id),
+              ),
             ),
           );
         },
