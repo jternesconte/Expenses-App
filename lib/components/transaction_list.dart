fcac1296f3c81551f85532966850109e22d58a8e
João Pedro Ternes Conte <joaopedrotc04@gmail.com>
2024-01-08 21:39:12 -0300

    improvement: Changed some size styles of code

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -10,68 +10,65 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 600,
-      child: transactions.isEmpty
-        ? Column(
-        children: [
-            const SizedBox(height: 20),
-            Text(
-              "No Transaction Registered",
-              style: Theme.of(context).textTheme.titleLarge,
+    return transactions.isEmpty
+      ? Column(
+      children: [
+          const SizedBox(height: 20),
+          Text(
+            "No Transaction Registered",
+            style: Theme.of(context).textTheme.titleLarge,
+          ),
+          const SizedBox(height: 20),
+          SizedBox(
+            height: 200,
+            child: Image.asset(
+              "assets/images/waiting.png",
+              fit: BoxFit.cover,
             ),
-            const SizedBox(height: 20),
-            SizedBox(
-              height: 200,
-              child: Image.asset(
-                "assets/images/waiting.png",
-                fit: BoxFit.cover,
-              ),
-            ),
-          ],
-        )
-      : ListView.builder(
-        itemCount: transactions.length,
-        itemBuilder: (ctx, index) {
-          final tr = transactions[index];
-          return Card(
-            elevation: 5,
-            margin: const EdgeInsets.symmetric(
-              vertical: 8,
-              horizontal: 5,
-            ),
-            child: ListTile(
-              leading: CircleAvatar(
-                backgroundColor: Colors.green,
-                radius: 30,
-                child: Padding(
-                  padding: const EdgeInsets.all(6),
-                  child: FittedBox(
-                    child: Text(
-                      "R\$${tr.value}",
-                      style: const TextStyle(
-                        color: Colors.white,
-                      ),
+          ),
+        ],
+      )
+    : ListView.builder(
+      itemCount: transactions.length,
+      itemBuilder: (ctx, index) {
+        final tr = transactions[index];
+        return Card(
+          elevation: 5,
+          margin: const EdgeInsets.symmetric(
+            vertical: 8,
+            horizontal: 5,
+          ),
+          child: ListTile(
+            leading: CircleAvatar(
+              backgroundColor: Colors.green,
+              radius: 30,
+              child: Padding(
+                padding: const EdgeInsets.all(6),
+                child: FittedBox(
+                  child: Text(
+                    "R\$${tr.value}",
+                    style: const TextStyle(
+                      color: Colors.white,
                     ),
                   ),
                 ),
               ),
-              title: Text(
-                tr.title,
-                style: Theme.of(context).textTheme.titleLarge,
-              ),
-              subtitle: Text(
-                DateFormat("d MMM y").format(tr.date)
-              ),
-              trailing: IconButton(
-                icon: const Icon(Icons.delete),
-                color: Theme.of(context).colorScheme.error,
-                onPressed: () => onRemove(tr.id),
-              ),
             ),
-          );
-        },
-      ),
+            title: Text(
+              tr.title,
+              style: Theme.of(context).textTheme.titleLarge,
+            ),
+            subtitle: Text(
+              DateFormat("d MMM y").format(tr.date)
+            ),
+            trailing: IconButton(
+              icon: const Icon(Icons.delete),
+              color: Theme.of(context).colorScheme.error,
+              onPressed: () => onRemove(tr.id),
+            ),
+          ),
+        );
+      },
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -95,8 +95,8 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
+
+    final appBar = AppBar(
         title: const Text("Personal Expenses"),
         actions: [
           IconButton(
@@ -105,13 +105,25 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
         ],
         backgroundColor: Theme.of(context).colorScheme.primary,
-      ),
+      );
+
+    final availableHeight = MediaQuery.of(context).size.height
+    - appBar.preferredSize.height - MediaQuery.of(context).padding.top;
+
+    return Scaffold(
+      appBar: appBar,
       body: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            Chart(_recentTransactions),
-            TransactionList(_transactions, _removeTransaction),
+            SizedBox(
+              height: availableHeight * 0.3,
+              child: Chart(_recentTransactions),
+            ),
+            SizedBox(
+              height: availableHeight * 0.7,
+              child: TransactionList(_transactions, _removeTransaction),
+            ),
           ],
         ),
       ),
