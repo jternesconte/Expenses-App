b304b5014b770d2682cd727023cada63b9303c09
João Pedro Ternes Conte <joaopedrotc04@gmail.com>
2024-02-15 17:00:23 -0300

    Feat: Create a button to show or not the chart and the list.

--- lib/main.dart
+++ lib/main.dart
@@ -96,10 +96,20 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    bool isLandscape = MediaQuery.of(context).orientation == Orientation.landscape;
 
     final appBar = AppBar(
         title: const Text("Personal Expenses"),
         actions: [
+          if (isLandscape)
+            IconButton(
+              icon: Icon(_showChart ? Icons.list : Icons.show_chart),
+              onPressed: () => {
+                setState(() {
+                  _showChart = !_showChart;
+                }),
+              },
+            ),          
           IconButton(
             icon: const Icon(Icons.add),
             onPressed: () => _openTransactionFormModal(context),
@@ -117,26 +127,28 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text("Exibir Gráfico"),
-                Switch(
-                  value: _showChart,
-                  onChanged: (value) {
-                    setState(() {
-                      _showChart = value;
-                    });
-                  },
-                ),
-              ],
-            ),
-            _showChart
-              ? SizedBox(
-                height: availableHeight * 0.3,
+            if(isLandscape)
+              // Row(
+              //   mainAxisAlignment: MainAxisAlignment.center,
+              //   children: [
+              //     const Text("Exibir Gráfico"),
+              //     Switch(
+              //       value: _showChart,
+              //       onChanged: (value) {
+              //         setState(() {
+              //           _showChart = value;
+              //         });
+              //       },
+              //     ),
+              //   ],
+              // ),
+            if (_showChart || !isLandscape)
+              SizedBox(
+                height: availableHeight * (isLandscape ? 0.7 : 0.3),
                 child: Chart(_recentTransactions),
-              )
-              : SizedBox(
+              ),
+            if (!_showChart || !isLandscape)
+              SizedBox(
                 height: availableHeight * 0.7,
                 child: TransactionList(_transactions, _removeTransaction),
               ),
