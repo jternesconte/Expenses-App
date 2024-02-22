79992b6c1d10b9deed369260a7ad645f0ab65046
João Pedro Ternes Conte <joaopedrotc04@gmail.com>
2024-02-21 21:41:09 -0300

    Refactor: Changed the padding in form

--- lib/components/transaction_form.dart
+++ lib/components/transaction_form.dart
@@ -46,66 +46,73 @@ class _TransactionFormState extends State<TransactionForm> {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 5,
-      child: Padding(
-        padding: const EdgeInsets.all(10),
-        child: Column(
-          children: [
-            TextField(
-              controller: _titleController,
-              onSubmitted: (_) => _submitForm(),
-              decoration: const InputDecoration(
-                labelText: "Title",
+    return SingleChildScrollView(
+      child: Card(
+        elevation: 5,
+        child: Padding(
+          padding: EdgeInsets.only(
+            top: 10,
+            right: 10,
+            left: 10,
+            bottom: 10 + MediaQuery.of(context).viewInsets.bottom,
+          ),
+          child: Column(
+            children: [
+              TextField(
+                controller: _titleController,
+                onSubmitted: (_) => _submitForm(),
+                decoration: const InputDecoration(
+                  labelText: "Title",
+                ),
               ),
-            ),
-            TextField(
-              controller: _valueController,
-              keyboardType:
-              const TextInputType.numberWithOptions(decimal: true),
-              onSubmitted: (_) => _submitForm(),
-              decoration: const InputDecoration(
-                labelText: "Price",
-              ),                    
-            ),
-            SizedBox(
-              height: 70,
-              child: Row(
-                children: <Widget>[
-                  Expanded(
-                    child: Text(
-                      _selectedDate == null
-                        ? "No date selected!"
-                        : "Selected Date: ${DateFormat("dd/MM/y").format(_selectedDate!)}",
-                    ),
-                  ),  
-                  TextButton(
-                    onPressed: _showDatePicker,
-                    child: const Text(
-                      "Select date",
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
+              TextField(
+                controller: _valueController,
+                keyboardType:
+                const TextInputType.numberWithOptions(decimal: true),
+                onSubmitted: (_) => _submitForm(),
+                decoration: const InputDecoration(
+                  labelText: "Price",
+                ),                    
+              ),
+              SizedBox(
+                height: 70,
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: Text(
+                        _selectedDate == null
+                          ? "No date selected!"
+                          : "Selected Date: ${DateFormat("dd/MM/y").format(_selectedDate!)}",
+                      ),
+                    ),  
+                    TextButton(
+                      onPressed: _showDatePicker,
+                      child: const Text(
+                        "Select date",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
                     ),
+                  ],
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: <Widget>[
+                  ElevatedButton(
+                    onPressed: _submitForm,
+                    child: const Text(
+                      "New Transaction",
+                      style: TextStyle(
+                        color: Colors.white,
+                    ),
+                      ),
                   ),
                 ],
               ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: <Widget>[
-                ElevatedButton(
-                  onPressed: _submitForm,
-                  child: const Text(
-                    "New Transaction",
-                    style: TextStyle(
-                      color: Colors.white,
-                  ),
-                    ),
-                ),
-              ],
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
