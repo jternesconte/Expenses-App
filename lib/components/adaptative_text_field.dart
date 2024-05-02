975dc911930b66320701e2ecded8aa9bdfcbf57e
João Pedro Ternes Conte <joaopedrotc04@gmail.com>
2024-05-01 21:38:33 -0300

    feat: created a adaptative style for both OS

---
+++ lib/components/adaptative_text_field.dart
@@ -0,0 +1,47 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class AdaptativeTextField extends StatelessWidget {
+
+  final String? label;
+  final TextInputType keyboardType;
+  final TextEditingController? controller;
+  final Function(String)? onSubmitted;
+
+  const AdaptativeTextField({super.key, 
+    this.label,
+    this.controller,
+    this.keyboardType = TextInputType.text,
+    this.onSubmitted,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Platform.isIOS
+    ? Padding(
+      padding: const EdgeInsets.only(
+        bottom: 10,
+      ),
+    child: CupertinoTextField(
+      controller: controller,
+      keyboardType: keyboardType,
+      onSubmitted: onSubmitted,
+      placeholder: label,
+      padding: const EdgeInsets.symmetric(
+        horizontal: 6,
+        vertical: 12,
+      ),
+    ),
+  )
+        : TextField(
+      controller: controller,
+        keyboardType: keyboardType,
+        onSubmitted: onSubmitted,
+        decoration:  InputDecoration(
+          labelText: label,
+        ),        
+    );
+  }
+}
